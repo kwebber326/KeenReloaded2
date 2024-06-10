f94e1102e0da469e3d854bb45a1e9e7b3679db4d
kjw2@comcast.net <kjw2@comcast.net>
2024-06-09 18:38:51 -0700

    changed confirmation message on closing map maker

--- KeenReloaded2/MapMaker.cs
+++ KeenReloaded2/MapMaker.cs
@@ -786,7 +786,7 @@ namespace KeenReloaded2
         private void MapMaker_FormClosing(object sender, FormClosingEventArgs e)
         {
             if (_mapHasUnsavedChanges
-           && MessageBox.Show("This map has unsaved changes, and this action will override those changes. Continue?", "Unsaved Changes", MessageBoxButtons.YesNo, MessageBoxIcon.Warning) == DialogResult.No)
+           && MessageBox.Show("This map has unsaved changes. Closing the map maker will cause you to lose these changes. Continue?", "Unsaved Changes", MessageBoxButtons.YesNo, MessageBoxIcon.Warning) == DialogResult.No)
             {
                 e.Cancel = true;
             }
