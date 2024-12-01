bd714dac72206eef7f40e77925e103bf846028ed
kjw2@comcast.net <kjw2@comcast.net>
2024-12-01 15:20:28 -0700

    added a lot more backgrounds and animations

--- KeenReloaded2/Properties/Resources.Designer.cs
+++ KeenReloaded2/Properties/Resources.Designer.cs
@@ -873,6 +873,26 @@ namespace KeenReloaded2.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap bg_keen6_caution_sign1 {
+            get {
+                object obj = ResourceManager.GetObject("bg_keen6_caution_sign1", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap bg_keen6_dome_background_eye_down {
+            get {
+                object obj = ResourceManager.GetObject("bg_keen6_dome_background_eye_down", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap Black_Flag {
             get {
                 object obj = ResourceManager.GetObject("Black_Flag", resourceCulture);
