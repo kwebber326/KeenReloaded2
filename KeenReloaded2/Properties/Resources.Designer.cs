92c15cc8dae49065eab644cb8269a6e4854564f8
kjw2@comcast.net <kjw2@comcast.net>
2025-01-13 15:21:31 -0700

    added water hazard depth image

--- KeenReloaded2/Properties/Resources.Designer.cs
+++ KeenReloaded2/Properties/Resources.Designer.cs
@@ -16573,6 +16573,16 @@ namespace KeenReloaded2.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap keen6_water_hazard_depth {
+            get {
+                object obj = ResourceManager.GetObject("keen6_water_hazard_depth", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap keen6_water_hazard_left1 {
             get {
                 object obj = ResourceManager.GetObject("keen6_water_hazard_left1", resourceCulture);
